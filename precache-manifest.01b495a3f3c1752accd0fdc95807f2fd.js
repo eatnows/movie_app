1f4f1fbd12460845254adcd60e4bb1bddd0d24e9
eatnows <gagyasindev@gmail.com>
2020-01-09 20:38:32 +0900

    Updates

---
+++ precache-manifest.01b495a3f3c1752accd0fdc95807f2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5da4155f14f21b824be99b26c058c6",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "77855c3ac63e82624126",
+    "url": "/movie_app/static/css/main.739d67c4.chunk.css"
+  },
+  {
+    "revision": "99b9d2a5fe5280c92ccd",
+    "url": "/movie_app/static/js/2.884192fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.884192fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77855c3ac63e82624126",
+    "url": "/movie_app/static/js/main.555bf58b.chunk.js"
+  },
+  {
+    "revision": "76273ac08d88daca9a2a",
+    "url": "/movie_app/static/js/runtime-main.36ba6c45.js"
+  }
+]);
